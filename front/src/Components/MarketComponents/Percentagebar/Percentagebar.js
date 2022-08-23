8aea6ccf66a4ebe7ce1d18cffa43ed7a5755b006
hsleenxsgl <hslee@nextsingular.com>
2022-08-23 21:37:49 +0900

    percentagebar 수정

--- front/src/Components/MarketComponents/Percentagebar/Percentagebar.js
+++ front/src/Components/MarketComponents/Percentagebar/Percentagebar.js
@@ -4,16 +4,22 @@ import './Percentagebar.css';
 import testtopicsdata from "../../../testdb/topics.json"
 
 export default function Percentagebar() {
-    console.log(testtopicsdata.topics[0].bet_info[0].coin)
-    return(
+    let agreeWidth = (testtopicsdata.topics[0].bet_info[0].coin/(testtopicsdata.topics[0].bet_info[0].coin+testtopicsdata.topics[0].bet_info[1].coin)).toFixed(2)*100
+    let disagreeWidth = 100 - agreeWidth
+    let agreeStyle = {width: `${agreeWidth}%` }
+    let disagreeStyle = {width: `${disagreeWidth}%` }
 
+    return(
         <div className='wholebox'>
-            <div className='agreedbox'>
-                {(testtopicsdata.topics[0].bet_info[0].coin/(testtopicsdata.topics[0].bet_info[0].coin+testtopicsdata.topics[0].bet_info[1].coin)).toFixed(2)*100}%
+            <div className='agreedbox' style={ agreeStyle }>
+                { agreeWidth }%
             </div>
-            <div className='disagreedbox'>
-                {(testtopicsdata.topics[0].bet_info[1].coin/(testtopicsdata.topics[0].bet_info[0].coin+testtopicsdata.topics[0].bet_info[1].coin)).toFixed(2)*100}%
+            <div className='disagreedbox' style={ disagreeStyle }>
+                { disagreeWidth }%
             </div>
+            <script>
+            </script>
         </div>
+        
     )
 }
